06df0390727453ff2b40ada5813e2e0882d0190a
AEmerson-Moura <emersonsilver@outlook.com.br>
2024-11-12 18:28:28 -0300

    Game seed

--- src/main/java/com/devsuperior/dslist/entities/Game.java
+++ src/main/java/com/devsuperior/dslist/entities/Game.java
@@ -23,9 +23,13 @@ public class Game {
 	private Integer year;
 	private String genre;
 	private String platforms;
-	private Double scores;
+	private Double score;
 	private String imgUrl;
+	
+	@Column(columnDefinition = "TEXT")
 	private String shortDescription;
+	
+	@Column(columnDefinition = "TEXT")
 	private String longDescription;
 	
 	
@@ -34,14 +38,14 @@ public class Game {
 	}
 
 
-	public Game(Long id, String title, Integer year, String genre, String platforms, Double scores, String imgUrl,
+	public Game(Long id, String title, Integer year, String genre, String platforms, Double score, String imgUrl,
 			String shortDescription, String longDescription) {
 		this.id = id;
 		this.title = title;
 		this.year = year;
 		this.genre = genre;
 		this.platforms = platforms;
-		this.scores = scores;
+		this.score = score;
 		this.imgUrl = imgUrl;
 		this.shortDescription = shortDescription;
 		this.longDescription = longDescription;
@@ -98,13 +102,13 @@ public class Game {
 	}
 
 
-	public Double getScores() {
-		return scores;
+	public Double getScore() {
+		return score;
 	}
 
 
-	public void setScores(Double scores) {
-		this.scores = scores;
+	public void setScore(Double score) {
+		this.score = score;
 	}
 
 
